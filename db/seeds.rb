e452709423207a9ffa3828a322ff4164a4d0df66
Mark Mounsey <mark.mounsey0512@gmail.com>
2021-03-23 19:23:59 +0100

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,29 @@ require 'json'
 require 'open-uri'
 
 puts 'cleaning the database'
+Cocktail.destroy_all
 Ingredient.destroy_all
+Dose.destroy_all
 
-letters_of_the_alphabet = ('a'..'c').to_a
+# Limited to letters a - t. Free API doesn't allow full use of there database.
+letters_of_the_alphabet = ('a'..'t').to_a
 
 letters_of_the_alphabet.each do |letter|
+  puts "Adding all cocktails and ingredients that start with #{letter.upcase}"
   url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}"
   drinks_serialized = open(url).read
   json_file = JSON.parse(drinks_serialized)
 
   json_file['drinks'].each do |drink|
+    cocktail = Cocktail.new(name: drink['strDrink'])
+    cocktail.valid? ? cocktail.save! : next
     ingredient = Ingredient.new(name: drink['strIngredient1'])
-    if ingredient.valid?
-      ingredient.save!
-    else
-      next
-    end
+    ingredient.valid? ? ingredient.save! : next
+    dose = Dose.new(description: drink['strMeasure1'])
+    dose.ingredient = ingredient
+    dose.cocktail = cocktail
+    dose.valid? ? dose.save! : next
   end
 end
+
+puts 'Database seeded'
